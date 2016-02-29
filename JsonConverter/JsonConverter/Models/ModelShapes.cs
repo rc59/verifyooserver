bbadbc9490a00e80c4a90d323943a6af1edd4e27
ivgeni chertok <chertok1@gmail.com>
2016-02-29 15:00:46 +0200

    new object

---
+++ JsonConverter/JsonConverter/Models/ModelShapes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using MongoDB.Bson;
+
+namespace JsonConverter.Models
+{
+    [DataContract]
+
+    class ModelShapes
+    {
+        public ObjectId _id { get; set; }
+
+        public string GcmToken { get; set; }
+
+        public bool IsSource { get; set; }
+
+        public double ScreenHeight { get; set; }
+
+        public double ScreenWidth { get; set; }
+
+        public string Name { get; set; }
+
+        public string ModelName { get; set; }
+
+        public string DeviceId { get; set; }
+
+        public string OS { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public double __v { get; set; }
+
+        public double Xdpi { get; set; }
+
+        public double Ydpi { get; set; }
+
+        //public string State { get; set; }
+
+        //public string Company { get; set; }
+
+        public List<ModelShape> ExpShapeList;
+
+        public string toString()
+        {
+            string result = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}", Created.ToShortDateString(), Created.ToLongTimeString(), Created.Millisecond.ToString(), Name, ModelName, DeviceId, OS, IsSource.ToString(), ScreenHeight.ToString(), ScreenWidth.ToString(), Xdpi.ToString(),Ydpi.ToString());
+            return result;
+        }
+    }
+}
