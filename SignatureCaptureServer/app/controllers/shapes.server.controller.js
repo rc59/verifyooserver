455e26388794805e1280620461f22aca30bfe421
Roy Dalal <dalal.roy@gmail.com>
2016-12-31 15:53:04 +0200

    Stable

--- SignatureCaptureServer/app/controllers/shapes.server.controller.js
+++ SignatureCaptureServer/app/controllers/shapes.server.controller.js
@@ -4,11 +4,50 @@
     _ = require('lodash'),
     fs = require('fs');
 
+exports.createTemplateDemoAuth = function(req, res) {
+    var dataString =  JSON.stringify(req.body);
+    if(dataString.length > 2) {
+        fs.writeFile("/temp/signatures/signature.json", dataString, function(err) {
+            if(err) {
+                res.status(400).send({ message: false });
+            }
+            else {
+                var readCounter = 0;
+                var readFuncVar = setInterval(function() {
+                    fs.readFile("/temp/signatures/result.json", 'utf8', function(err, data) {
+                        if (err) {
+                            readCounter++;
+                            if(readCounter > 10) {
+                                clearInterval(readFuncVar);
+                                res.status(400).send({ message: false });
+                            }
+                        }
+                        else {
+                            clearInterval(readFuncVar);
+                            var score = "@" + data + "@";
+                            res.status(200).send({ message: score });
+
+                            fs.unlink("/temp/signatures/result.json", 'utf8', function(err) {
+
+                            });
+                        }
+                    });
+                }, 500);
+            }
+
+            console.log("The file was saved!");
+        });
+    }
+    else {
+        res.status(200).send({ message: true });
+    }
+};
+
 exports.createTemplateDemo = function(req, res) {
     var dataString =  JSON.stringify(req.body);
-    fs.writeFile("/temp/signature.json", dataString, function(err) {
+    fs.writeFile("/temp/signatures/signatureTest.json", dataString, function(err) {
         if(err) {
-                res.status(400).send({ message: false });
+            res.status(400).send({ message: false });
         }
         else {
             res.status(200).send({ message: true });
--- SignatureCaptureServer/app/routes/shapes.server.routes.js
+++ SignatureCaptureServer/app/routes/shapes.server.routes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
     var shapes = require('../../app/controllers/shapes');
 
     app.route('/shapes/createTemplateDemo').post(shapes.createTemplateDemo);
+    app.route('/shapes/createTemplateDemoAuth').post(shapes.createTemplateDemoAuth);
 };
